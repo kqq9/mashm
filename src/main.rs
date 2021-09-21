fda75c5d86aa58414de335a07ea9f5b757712fa0
Zenas Zhu <zenaszhu02@gmail.com>
2021-09-21 10:34:30 -0700

    Added a length option yay

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-const KEYS: &str = "asdfghjkl;"; // TODO: keys from other rows sometimes
+use gumdrop::Options;
+
+const KEYS: &str = "asdfghjkl;";
+
+#[derive(Options)]
+struct MashmOptions {
+	help: bool,
+	#[options(meta="N")]
+	length: Option<u16>,
+	// TODO: use upper row maybe
+}
 
 fn main() {
+	let opts = MashmOptions::parse_args_default_or_exit();
+	let mash_len = opts.length.unwrap_or(20);
 	let mut s = [100; 10];
 	let mut sum = 1000;
-	for _ in 0..20 {
+	for _ in 0..mash_len {
 		let mut r = rand::random::<i32>().rem_euclid(sum);
 		let mut keyi = 9;
 		for i in 0..10 {
@@ -21,6 +33,10 @@ fn main() {
 		}
 		sum -= s[keyi] % 9;
 		s[keyi] = 0;
+		if sum < 900 {
+			s.iter_mut().for_each(|x| *x += 20);
+			sum += 200;
+		}
 		print!("{}", &KEYS[keyi..keyi+1]);
 	}
 	println!();
